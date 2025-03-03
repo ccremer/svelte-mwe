57061ce4bf3718a765a9d875fa7523da041e8dc7
ccremer <github.account@chrigel.net>
2025-03-03 18:53:19 +0100

    Add new project

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,16 @@
+import { PostHog } from "posthog-node";
+
+export async function load() {
+  const posthog = new PostHog("<API_KEY>", {
+    host: "https://us.i.posthog.com",
+  });
+
+  posthog.capture({
+    distinctId: "hog-test",
+    event: "loaded_svelte_app",
+  });
+
+  posthog.captureException(new Error("A random error"));
+
+  await posthog.shutdown();
+}
