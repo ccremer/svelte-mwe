b7e7cc7fce26ef3b36cfc1804d631a9cb3b192c6
ccremer <github.account@chrigel.net>
2025-04-23 16:06:51 +0200

    Setup kit-routes to reproduce issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+
+import {
+  kitRoutes,
+  type Options,
+  type RouteMappings,
+} from "vite-plugin-kit-routes";
+
+export const _kitRoutesConfig: Options<RouteMappings> = {
+  trailingSlash: "always",
+};
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit(), kitRoutes(_kitRoutesConfig)],
 });
